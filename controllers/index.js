77ce36489ab6f44c6da790b51d98a323d5228bc9
Ksmike <michael.copeland@news.co.uk>
2016-06-21 21:50:54 +0100

    Porter: added products, added product card and styles

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var IndexModel = require('../models/index');
+var request = require('request');
 
 
 module.exports = function (router) {
@@ -9,10 +10,13 @@ module.exports = function (router) {
 
     router.get('/', function (req, res) {
 
+    	request('http://127.0.0.1:3000/api/products', function(error, response, body) {
+    	    res.render('index', {
+    	        products: JSON.parse(body)
+    	    });
+    	});
 
 
-
-        res.render('index', model);
 
 
 
--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,15 @@
 'use strict';
+
+	var Porter = {
+		init: function () {
+			$('.product-grid').isotope({
+				itemSelector: '.product-wrapper'
+			});
+
+			console.log("wakka");
+		}
+	};
+
+$(function	() {
+	Porter.init();
+});
