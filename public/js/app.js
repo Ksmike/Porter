60c7c59f22212cd99c45db51bb9f4db0f84d1ba0
Ksmike <michael.copeland@news.co.uk>
2016-06-23 15:21:12 +0100

    NAP: removed hover style added getMoreProd fund

--- public/js/app.js
+++ public/js/app.js
@@ -7,6 +7,25 @@ var Porter = {
 		});
 
 		$('.backdrops').fadeOut('slow');
+		window.addEventListener('scroll', this.getMoreProducts.bind(this), false);
+	},
+	getMoreProducts: function () {
+		$.ajax({
+			url: 'http://127.0.0.1:3000/api/products',
+			type: 'GET',
+			dataType: 'json',
+			data: {
+				offset: 60,
+				limit: 200
+			}
+		})
+		.done(function (data) {
+			this.renderNewProducts(data);
+		}.bind(this));
+
+	},
+	renderNewProducts: function (products) {
+		console.log('products: ', products);
 	}
 };
 
